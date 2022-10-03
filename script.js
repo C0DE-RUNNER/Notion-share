98b009af829297269c13865ecb15a9ed399068bf
Sakshi Thakur <nehathakursakshi@gmail.com>
2022-10-03 12:12:39 +0530

    Create footer, fix CSS, add OSlash group

--- script.js
+++ script.js
@@ -3,19 +3,38 @@ const modal1 = document.querySelector(".modal-1");
 const modal2 = document.querySelector(".modal-2");
 const inputField1 = document.querySelector(".input1");
 const inputField2 = document.querySelector(".input2");
+const inviteButton1 = document.querySelector(".invite-1");
 
 inputField1.addEventListener("click", displayShareForm);
 shareButton.addEventListener("click", hideShareForm);
+inviteButton1.addEventListener("click", displayShareForm);
+
+// storing the selected people and group
+let accessList = {};
+let mainList = {};
 
 function displayShareForm() {
-  // $("#multi-select").dropdown("clear");
-  //   document.querySelector(".select-1"). = -1;
+  // create a copy of multi-select dropdown "select" element
+  const multiDropdownCopy = document
+    .getElementById("multi-select")
+    .cloneNode(true);
+
+  // hide the displaying of previously selected items from the select-field
+  $(".select-1 > a").remove();
+  // remove the old multi-select dropdown with reduced number of options
+  $(".select-1 > #multi-select").remove();
+
+  // append the previously stored original dropdown with all options in place of above
+  document.querySelector(".select-1").appendChild(multiDropdownCopy);
+
+  // reset the accessList too
+  for (let member in accessList) delete accessList[member];
+
   modal1.classList.add("hidden");
   modal2.classList.remove("hidden");
 }
 
-// hide the people/group list for click anywhere on the screen
-
+// Return back to the main-view
 function hideShareForm() {
   modal1.classList.remove("hidden");
   modal2.classList.add("hidden");
@@ -26,32 +45,25 @@ $(".ui.dropdown").dropdown({
   allowAdditions: true,
 });
 
-// storing the selected people and group in an array
-let accessList = {};
-let mainList = {};
+// On people/group selection
 $("#multi-select").dropdown("setting", "onChange", function(values) {
   console.log(...values);
   // get the permission
-  let permission = document.querySelector(".select-2");
-  let value = permission.value;
+  let permission = document.querySelector(".select-2").value;
 
-  // if people are added and then removed, then update the accessList accordingly
-  if (value.length === 0) {
-    accessList = {};
-  } else {
-    // reset the accessList first
-    accessList = {};
-    // then update the list
-    values.forEach((el) => {
-      accessList[el] = value;
-    });
-  }
+  // reset the accessList first
+  for (let member in accessList) delete accessList[member];
+
+  // then update the list
+  values.forEach((el) => {
+    accessList[el] = permission;
+  });
 
   console.log(accessList);
 });
 
 // On the "invite (2)" button click, return to modal-1 and display the accessList with permission
-document.querySelector(".invite2").addEventListener("click", () => {
+document.querySelector(".invite-2").addEventListener("click", () => {
   // get the permission
   let permission = document.querySelector(".select-2");
   let value = permission.value;
@@ -89,7 +101,7 @@ function displayList() {
       div.appendChild(span);
 
       //Create array of options to be added
-      let accessTexts = ["Full access", "Can edit", "view", "No access"];
+      let accessTexts = ["Full access", "Can edit", "Can view", "No access"];
       let accessValues = ["full", "edit", "view", "none"];
 
       //Create and append select list
@@ -102,6 +114,7 @@ function displayList() {
         let option = document.createElement("option");
         option.value = accessValues[i];
         option.text = accessTexts[i];
+        // add the selected attribute
         if (mainList[key] === accessValues[i]) {
           option.setAttribute("selected", "");
         }
@@ -112,3 +125,26 @@ function displayList() {
     }
   }
 }
+
+// use event delegation to handle onchange event on dynamically added "select-3" tag
+document.getElementById("accessList").addEventListener("change", function(e) {
+  if (e.target) {
+    let key = e.target.parentElement.firstChild.innerText;
+    let value = e.target.value;
+    // update the permission for current individual/group in the mainList
+    accessList[key] = value;
+    // mainList[key] = value;
+    console.log(key, value, mainList);
+
+    // re-render the list
+    displayList();
+  }
+});
+
+// handle copy link click
+document.querySelector(".copy-btn").addEventListener("click", function(e) {
+    e.target.innerHTML = `<i class="fa fa-link" aria-hidden="true"></i> Copied link!`;
+    let url = e.target.getAttribute("href");
+    navigator.clipboard.writeText(url);
+  });
+  
